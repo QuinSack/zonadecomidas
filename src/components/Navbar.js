2ed7115f4616586eec866469ccc758bfe3ceeeb4
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-06-13 22:08:22 +0000

    did some work on the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Navbar = () => {
   return (
     <nav className='nav' sticky = 'top'>
         <Link to='/' className='home-page'>Zona de Comidas</Link>
+        <div className='searchsection'>
+          <input type='text' className='searchbar' placeholder='Search for your favorite meals, categories and more' />
+          <button className='searchbutton'><FontAwesomeIcon icon={faSearch} color='white'></FontAwesomeIcon></button>
+        </div>
         <ul>
             <li>
               <Link to='/shop'>Browse Meals</Link>
