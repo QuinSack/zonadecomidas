4e44af94893d85494ae71c34a35fc7f836799ec5
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-06-23 18:41:52 +0000

    finished up signin page and started with signup

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,7 +2,30 @@ import React from 'react'
 
 const SignUpForm = () => {
   return (
-    <div>SignUpForm</div>
+    <div className='signupformcontainer'>
+        <div className='signupformwelcome'>
+            <h3>Some fancy UI down below...</h3>
+        </div>
+        <form className='signupform'>
+            <div className='forminput'>
+                <section className='emailsection'>
+                    <div className='emaillabel'>
+                        <label htmlFor='email'>Email: </label>
+                    </div>
+                    <div className='emailinput'>
+                        <input type='email' placeholder='Enter your email' id='email' name='email' />
+                    </div>
+                </section>
+                <section className='passwordsection'> 
+                    <label htmlFor='password' className='passwordlabel'>Password: </label>
+                    <input type='password' placeholder='Enter your password' id='password' name='password' className='passwordinput' />
+                </section>
+            </div>   
+            <section>
+                <button type='submit' className='signupbtn'>Create Account</button>
+            </section>
+        </form>
+    </div>
   )
 }
 
